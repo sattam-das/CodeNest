5d6648f9f8949add67116d5fe5bc46e6c64bd280
Sattam Das <sattamwk06@gmail.com>
2026-02-13 21:53:47 +0530

    file delete

--- week_2.py
+++
@@ -1,8 +0,0 @@
-def count_substrings(S):
-    c = {}
-    for x in S: 
-        c[x] = c.get(x, 0) + 1
-    return sum(v * (v + 1) // 2 for v in c.values())
-
-if __name__ == "__main__":
-    print(count_substrings("abcdefedcba"))
